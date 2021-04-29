c989289b2438d8ef5894628c7b444c0521ece6ca
LyTranHoangHieu <73414103+LyTranHoangHieu@users.noreply.github.com>
2021-04-29 22:03:34 +0700

    Update SelectionSort.cpp

--- O(N^2)/SelectionSort.cpp
+++ O(N^2)/SelectionSort.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+
 using namespace std;
 
 void swap(int &x, int &y){
@@ -6,6 +7,7 @@ void swap(int &x, int &y){
 	x = y;
 	y = temp;
 }
+
 void selectionSort(int a[], int n){
 	int i = 0, posMin;
 	while(i < n-1){
@@ -21,29 +23,39 @@ void selectionSort(int a[], int n){
 		i++;
 	}
 }
+
 void readArray(int a[], int n){
 	cout << "Enter " << n << " Element's content in any order" << endl;
 	for(int i = 0; i < n; i++)
 		cin >> a[i];		
 }
+
 void printArray(int a[], int n){
 	for(int i = 0; i < n; i++)
 		cout << a[i] << '\t';
 	cout << endl;
 }
+
 int main(){
 	int n;
+	
 	do
 	{
 		cout << "How many elements are there in Array? ";
 		cin >> n;
 	} while(n <= 0);
+	
 	int a[n];
+	
 	readArray(a, n);
+	
 	cout << "=====Given Array=====" << endl;
 	printArray(a, n);
+	
 	selectionSort(a, n);
+	
 	cout << "=====Selection Sort Array=====" << endl;
 	printArray(a, n);
+	
 	return 0;
 }
