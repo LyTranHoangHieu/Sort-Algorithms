d8779998c8fe39175e432337cdd48dc9d6ca6b3d
LyTranHoangHieu <73414103+LyTranHoangHieu@users.noreply.github.com>
2021-04-29 22:12:51 +0700

    Add files via upload

---
+++ O(NlogN)/QuickSort_Array.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+
+using namespace std;
+
+void swap(int &x, int &y){
+	int temp = x;
+	x = y;
+	y = temp;
+}
+
+void partitionSort(int a[], int first, int last){
+	if(first >= last)
+		return;
+	int x = a[(first+last)/2];
+	int i = first, j = last;
+	do{
+		while(a[i] < x)
+			i++;
+		while(a[j] > x)
+			j--;
+		if(i <= j){
+			swap(a[i], a[j]);
+			i++;
+			j--;
+		}
+	}while(i <= j);
+	partitionSort(a, first, j);
+	partitionSort(a, i, last);
+}
+
+void quickSort(int a[], int n){
+	partitionSort(a, 0, n-1);
+}
+
+void readArray(int a[], int n){
+	cout << "Enter " << n << " Element's content in any order" << endl;
+	for(int i = 0; i < n; i++)
+		cin >> a[i];		
+}
+
+void printArray(int a[], int n){
+	for(int i = 0; i < n; i++)
+		cout << a[i] << '\t';
+	cout << endl;
+}
+
+int main(){
+	int n;
+	
+	do{
+		cout << "How many elements are there in Array? ";
+		cin >> n;
+	} while(n <= 0);
+	
+	int a[n];
+	
+	readArray(a, n);
+	
+	cout << "=====Given Array=====" << endl;
+	printArray(a, n);
+	
+	quickSort(a, n);
+	
+	cout << "=====Quick Sort Array=====" << endl;
+	printArray(a, n);
+	
+	return 0;
+}
